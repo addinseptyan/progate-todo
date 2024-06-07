05820169089298456b3bfa9d8b533cc6ca35fb51
Addin Septyan <addinseptyan31@gmail.com>
2024-06-07 22:34:44 +0700

    develop add todo feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import Todos from './components/Todos';
+import TodoForm from './components/TodoForm';
 
 function App() {
   const [todos, setTodos] = useState([
@@ -35,9 +36,29 @@ function App() {
     setTodos(newTodos);
   }
 
+  // const addTodo = () => {
+  //   console.log('This is addTodo Function')
+  // }
+
+  const addTodo = (todoTitle) => {
+    if (todoTitle === '') {
+      return
+    }
+
+    const newTodo = {
+      id: todos.length + 1,
+      title: todoTitle,
+      completed: false,
+    }
+
+    const updatedTodos = todos.concat(newTodo)
+    setTodos(updatedTodos)
+  }
+  
   return (
     <div style={styles.container}>
       <h1 style={styles.title}>My Todo List</h1>
+      <TodoForm addTodo={addTodo} /> 
       <Todos todos={todos} toggleCompleted={toggleCompleted} deleteTodo={deleteTodo}/>
     </div>
   )
@@ -47,6 +68,7 @@ const styles = {
   container: {
     textAlign: 'center',
     padding: '12px',
+    fontFamily: 'sans-serif'
   },
   title: {
     fontSize: '36px'
