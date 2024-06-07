eb3cfa90bb9234369fed1761486ceb5fe4667a62
Addin Septyan <addinseptyan31@gmail.com>
2024-06-07 21:35:52 +0700

    Develop toggle todo feature

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,15 +1,37 @@
-const TodoItem = ({todo}) => {
+const TodoItem = ({ todo, toggleCompleted }) => {
+    const getTodoTitleStyle = () => {
+        if (todo.completed === true) {
+            return { textDecoration: 'line-through' }
+        } else {
+            return { textDecoration: 'none' }
+        }
+    }
+
     return (
-    <div style={styles.todoItem}>
-        <p>{todo.title}</p>
-    </div>
+        <div style={styles.todoItem}>
+            <input
+                type="checkbox"
+                style={styles.checkbox}
+                onChange={() => { toggleCompleted(todo.id) }}
+            />
+            <p style={getTodoTitleStyle()}>{todo.title}</p>
+        </div>
     )
 }
 
 const styles = {
     todoItem: {
         border: '2px solid #f4f4f4',
-        fontSize: '24px'
+        fontSize: '24px',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    checkbox: {
+        marginRight: '10px',
+        height: '18px',
+        width: '18px',
+        display: 'inline'
     }
 }
 
